46ccc15063f9de277b6f046fc9fa3875e1ec44e0
George Dilthey <george.dilthey@gmail.com>
2021-05-17 22:36:15 -0400

    finished edit functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -13,30 +13,29 @@ class StoriesController < ApplicationController
     erb :"/stories/show.html"
   end
 
+  get "/stories/:id/edit" do
+    @story = Story.find_by_id(params[:id])
+    @author = User.find_by_id(@story.author_id)
+    erb :"/stories/edit.html"
+  end
+
   patch "/stories/:id" do
     story = Story.find_by_id(params[:id])
-    word = params[:word]
-    current_text = story.text
-    new_text = "#{current_text} #{word}"
-    story.update(:text => new_text)
-
-    redirect "/stories/#{story.id}"
-
+    if params[:word]
+        word = params[:word]
+        current_text = story.text
+        new_text = "#{current_text} #{word}"
+        story.update(:text => new_text)
+        redirect "/stories/#{story.id}"
+    elsif params[:completed]
+        story.update(:completed=>params[:completed])
+        redirect "/stories/#{story.id}"
+    else
+        story.update(:text=>params[:text], :title=>params[:title])
+        redirect "/stories/#{story.id}"
+    end
   end
 
-  patch "/stories/complete/:id" do
-    story = Story.find_by_id(params[:id])
-    story.update(:completed => true)
-
-    redirect "/stories/#{story.id}"
-  end
-
-  patch "/stories/incomplete/:id" do
-    story = Story.find_by_id(params[:id])
-    story.update(:completed => false)
-
-    redirect "/stories/#{story.id}"
-  end
 
   
 
